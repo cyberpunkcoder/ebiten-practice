d37c21fd29d0f2e2aab530c9c36702aaa7948aa2
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-10-25 14:08:23 -0700

    finished showimage tutorial

--- showimage/main.go
+++ showimage/main.go
@@ -1,3 +1,9 @@
+/*
+author: cyberpunkprogrammer
+date: 10-25-2020
+tutorial: https://medium.com/@chrisandrews_76960/2d-game-development-in-golang-part-2-5372e9338488
+*/
+
 package main
 
 import (
@@ -12,14 +18,7 @@ var (
 	background *ebiten.Image
 )
 
-func update(screen *ebiten.Image) error {
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-	ebitenutil.DebugPrint(screen, "Hello world!")
-	return nil
-}
-
+// load background image from assets/space.png
 func init() {
 	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
 	if err != nil {
@@ -27,6 +26,21 @@ func init() {
 	}
 }
 
-func main() {
+// update the screen with background image
+func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(0, 0)
+	screen.DrawImage(background, op)
 
+	return nil
+}
+
+// main loop
+func main() {
+	if err := ebiten.Run(update, 640, 480, 1, "Hello world!"); err != nil {
+		log.Fatal(err)
+	}
 }
