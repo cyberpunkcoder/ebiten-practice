222d370fc235fbb38c0b97f1a0f528174869fea3
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-10-25 14:09:55 -0700

    added comments to helloworld

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,3 +1,9 @@
+/*
+author: cyberpunkprogrammer
+date: 10-25-2020
+tutorial: https://medium.com/@chrisandrews_76960/2d-game-development-in-golang-part-1-5e2c11a513ed
+*/
+
 package main
 
 import (
@@ -7,6 +13,7 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+// update the text hello world on the screen
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
@@ -15,6 +22,7 @@ func update(screen *ebiten.Image) error {
 	return nil
 }
 
+// main loop
 func main() {
 	if err := ebiten.Run(update, 320, 240, 2, "Hello world!"); err != nil {
 		log.Fatal(err)
