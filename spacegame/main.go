c9b6e44e0613931b2397d5f7013c7ff8e2b886b2
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-11-06 16:40:29 -0800

    player drawn on center of screen

--- spacegame/main.go
+++ spacegame/main.go
@@ -9,28 +9,60 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 var (
-	err                       error
-	screenWidth, screenHeight = 640, 480
-	background                *ebiten.Image
-	player                    *ebiten.Image
-	bullet                    *ebiten.Image
-	enemy                     *ebiten.Image
+	err                              error
+	screenWidth, screenHeight, scale = 640, 480, 2
+	backgroundImage                  *ebiten.Image
+	playerImage                      *ebiten.Image
+	bulletImage                      *ebiten.Image
+	enemyImage                       *ebiten.Image
+	player                           object
 )
+
+type object struct {
+	image            *ebiten.Image
+	xPos, yPos       float64
+	xSpd, ySpd, rSpd float64
+}
 
 func init() {
 	//ebiten.SetFullscreen(true)
+
+	// scale up pixel art for aesthetics
 	screenWidth, screenHeight = ebiten.ScreenSizeInFullscreen()
+	screenWidth /= scale
+	screenHeight /= scale
+
+	playerImage, _, err = ebitenutil.NewImageFromFile("assets/player.png", ebiten.FilterDefault)
+	player.image = playerImage
+
+	// position player at center of screen
+	playerImageX, playerImageY := player.image.Size()
+	player.xPos = float64((screenWidth / 2) - (playerImageX / 2))
+	player.yPos = float64((screenHeight / 2) - (playerImageY / 2))
+
+	// make player still
+	player.xSpd, player.ySpd, player.rSpd = 0, 0, 0
 }
 
 func update(screen *ebiten.Image) error {
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	playerOp := &ebiten.DrawImageOptions{}
+	playerOp.GeoM.Translate(player.xPos, player.yPos)
+	screen.DrawImage(player.image, playerOp)
+
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "Space Game"); err != nil {
+	if err := ebiten.Run(update, screenWidth, screenHeight, float64(scale), "Space Game"); err != nil {
 		log.Fatal(err)
 	}
 }
